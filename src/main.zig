cd1364c7d23c9a7d86ad9261d24b7e907a04a52e
namish <namish@tuta.io>
2025-01-20 15:34:29 +0530

    haha pfp

--- src/main.zig
+++ src/main.zig
@@ -10,14 +10,19 @@ pub fn main() !void {
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
+    // var cam = camera.Camera{ .aspect_ratio = 1, .image_width = 500 };
     var cam = camera.Camera{};
     cam.initialise();
 
     var world = H.HittableList.init(allocator);
     defer world.deinit();
 
+    // const avatar_head = H.Sphere.init(vector.Vec3{ 0, 0, -1 }, 0.5);
+    // const avatar_body = H.Sphere.init(vector.Vec3{ 0, -2, -1 }, 1.2);
+
     const sphere = H.Sphere.init(vector.Vec3{ 0, 0, -1 }, 0.5);
     const ground = H.Sphere.init(vector.Vec3{ 0, -100.5, -1 }, 100);
+
     try world.add(sphere);
     try world.add(ground);
 
